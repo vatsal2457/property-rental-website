90a536b8e4a0752ba18842856cc485412b74bc84
Vatsal Vishal <vatsalvishal003@gmail.com>
2025-03-11 15:13:13 +0530

    Updated About component

--- Frontend/src/components/About/About.jsx
+++ Frontend/src/components/About/About.jsx
@@ -14,9 +14,9 @@ function About() {
         <div className="bg-white border rounded-2xl flex flex-col  border-black w-4/5 mt-10 text-lg md:text-2xl py-10 px-3">
         <h1 className="font-bold text-xl md:text-4xl font-serif"> Hello!</h1>
         <div className="mt-4 flex space-x-2">
-        <h1> I am </h1><h1 className="font-bold">Aryan Kesarkar</h1>
+        <h1> I am </h1><h1 className="font-bold">Vatsal Vishal</h1>
         </div>
-        <p className="mt-8">I am currently pursuing a degree in Computer Engineering from Pune University.</p>
+        <p className="mt-8">I am graduated from mit academy of engineering and technology, Pune.</p>
         <p className="mt-8">My passion lies in the world of development, where I thrive on creating innovative solutions and exploring new technologies.</p>
         <p className="mt-8">With a strong foundation in computer engineering principles and a keen interest in development, I am dedicated to bringing cutting-edge ideas to life and continuously learning in this ever-evolving field.</p>
           
